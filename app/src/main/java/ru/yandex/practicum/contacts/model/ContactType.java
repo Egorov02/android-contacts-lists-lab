e3019406a8c6aae89a380aabeed0e6e3e8a94d3e
Sergey Egorov <java.egorov@gmail.com>
2023-03-23 14:35:57 +0300

    Добавил код в методы getContactTypes() и updateFilterContactTypes()

--- app/src/main/java/ru/yandex/practicum/contacts/model/ContactType.java
+++ app/src/main/java/ru/yandex/practicum/contacts/model/ContactType.java
@@ -12,5 +12,7 @@ public class ContactType {
 
     public static String[] getContactTypes() {
         // метод должен возвращать массив строк, перечисленных выше
+        String[] contactType = {TELEGRAM, WHATS_APP,VIBER, SIGNAL, THREEMA, PHONE, EMAIL};
+        return contactType;
     }
 }
--- app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/FilterContactTypeViewModel.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/FilterContactTypeViewModel.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.contacts.presentation.filter;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
@@ -60,12 +61,15 @@ public class FilterContactTypeViewModel extends BaseBottomSheetViewModel {
     }
 
     private void updateFilterContactTypes() {
-        // создайте массив строк. Для инициализации используйте метод ContactType.getContactTypes(),
+        String [] arrayTypes = ContactType.getContactTypes();// создайте массив строк. Для инициализации используйте метод ContactType.getContactTypes(),
         // который должен возвращать список всех доступных источников контактов
 
-        // создайте список типа FilterContactTypeUi и заполните его с помощью цикла forEach
-        // forEach должен бежать по строковому массиву, который вы создали ранее
-
+        ArrayList<FilterContactTypeUi> filterContactTypeUiList = new ArrayList<>();// создайте список типа FilterContactTypeUi и заполните его с помощью цикла forEach
+        filterContactTypeUiList.add(createAllSelectedItem(arrayTypes));
+        for (String arrayType : arrayTypes) {
+            filterContactTypeUiList.add(createFilterContactType(arrayType));
+        }// forEach должен бежать по строковому массиву, который вы создали ранее
+        filterContactTypesLiveDate.setValue(filterContactTypeUiList);
         // вызовите меотод setValue() у переменной filterContactTypesLiveDate и передайте в качестве аргументы ваш список типа FilterContactTypeUi
     }
 
